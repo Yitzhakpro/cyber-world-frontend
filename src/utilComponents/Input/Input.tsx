d7aacc4d721894e33a60ccf1b46f050ccbf3a412
Yitzhak <isako14@gmail.com>
2022-05-22 13:41:44 +0300

    added basic input util component

---
+++ src/utilComponents/Input/Input.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    test?: boolean;
+}
+
+function Input(props: InputProps): JSX.Element {
+    const { test = false } = props;
+
+    return <input />;
+}
+
+export default Input;
