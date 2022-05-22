20393ba1568548e1ddcb1d5501a21ade8721a121
Yitzhak <isako14@gmail.com>
2022-05-22 12:01:53 +0300

    added lint-staged with husky integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App(): JSX.Element {
-    return <div className="App">Somthing should be here...</div>;
+    return <div className="App">Somthing should be here....</div>;
 }
 
 export default App;
