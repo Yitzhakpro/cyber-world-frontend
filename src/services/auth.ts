6d31c8774a441e23473ef05bbfa3d1e5858d1de2
Yitzhak <isako14@gmail.com>
2022-05-24 15:08:41 +0300

    added basic auth service

---
+++ src/services/auth.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import config from '../config';
+
+/*
+    TODO: add return type to functions
+    TODO: add logic for return url
+*/
+
+const serverUrl = config.get('serverUrl');
+
+class AuthClient {
+    client: AxiosInstance;
+
+    constructor() {
+        this.client = axios.create({
+            baseURL: `${serverUrl}/api/auth`,
+            withCredentials: true,
+        });
+    }
+
+    async register(
+        email: string,
+        username: string,
+        birthDate: Date,
+        password: string,
+        rememberMe: boolean
+    ): Promise<AxiosResponse<any, any>> {
+        const registerResponse = await this.client.post('/register', { email, username, birthDate, password, rememberMe });
+
+        return registerResponse;
+    }
+
+    async login(email: string, password: string, rememberMe: boolean): Promise<AxiosResponse<any, any>> {
+        const loginResponse = await this.client.post('/login', { email, password, rememberMe });
+
+        return loginResponse;
+    }
+}
+
+const Auth = new AuthClient();
+export default Auth;
