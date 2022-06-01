441843f0231860dc03de768666f6d847791aa28f
Yitzhak <isako14@gmail.com>
2022-06-01 14:43:51 +0300

    added basic join room logic

--- src/components/ChatRoom/ChatRoom.tsx
+++ src/components/ChatRoom/ChatRoom.tsx
@@ -1,30 +1,53 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import SocketIoClient, { Socket } from 'socket.io-client';
+import config from '../../config';
+
+const { serverUrl } = config;
 
 function ChatRoom(): JSX.Element {
+    const params = useParams();
     const [socket, setSocket] = useState<Socket | null>(null);
-    const [msg, setMessage] = useState('');
+
+    const [joinStatus, setJoinStatus] = useState<{ joined: boolean; message?: string }>({ joined: false, message: '' });
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
-        const socketInstance = SocketIoClient('http://localhost:8080', { withCredentials: true });
+        const socketInstance = SocketIoClient(serverUrl, { withCredentials: true });
         setSocket(socketInstance);
+
+        socketInstance.emit('join_room', params.roomID);
+        socketInstance.on('joined_successfully', () => {
+            setJoinStatus({ joined: true });
+        });
+        socketInstance.on('join_failed', (reason: string) => {
+            setJoinStatus({ joined: false, message: reason });
+        });
+
         return () => {
             socketInstance.close();
         };
-    }, []);
+    }, [params.roomID]);
 
     const sendMsg = (): void => {
-        socket?.emit('message', msg);
+        socket?.emit('message', message);
         setMessage('');
     };
 
     return (
         <div>
             <h1>chat here</h1>
-            <input value={msg} onChange={(e) => setMessage(e.target.value)} />
-            <button type="button" onClick={sendMsg}>
-                send
-            </button>
+
+            {joinStatus.joined ? (
+                <>
+                    <input value={message} onChange={(e) => setMessage(e.target.value)} />
+                    <button type="button" onClick={sendMsg}>
+                        send
+                    </button>
+                </>
+            ) : (
+                <p>{joinStatus.message}</p>
+            )}
         </div>
     );
 }
