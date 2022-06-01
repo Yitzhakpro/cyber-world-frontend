2cbf046dec1c583ce2e29f124e93359da673d43f
Yitzhak <isako14@gmail.com>
2022-06-01 15:52:25 +0300

    added MessageInput component & basic receive message logic

--- src/components/ChatRoom/ChatRoom.tsx
+++ src/components/ChatRoom/ChatRoom.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import SocketIoClient, { Socket } from 'socket.io-client';
+import MessageInput from './MessageInput';
 import config from '../../config';
 
 const { serverUrl } = config;
@@ -10,12 +11,13 @@ function ChatRoom(): JSX.Element {
     const [socket, setSocket] = useState<Socket | null>(null);
 
     const [joinStatus, setJoinStatus] = useState<{ joined: boolean; message?: string }>({ joined: false, message: '' });
-    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState<string[]>([]);
 
     useEffect(() => {
         const socketInstance = SocketIoClient(serverUrl, { withCredentials: true });
         setSocket(socketInstance);
 
+        // join room logic
         socketInstance.emit('join_room', params.roomID);
         socketInstance.on('joined_successfully', () => {
             setJoinStatus({ joined: true });
@@ -26,12 +28,28 @@ function ChatRoom(): JSX.Element {
 
         return () => {
             socketInstance.close();
+            // join logic
+            socketInstance.off('joined_successfully');
+            socketInstance.off('join_failed');
+            // messages logic
+            socketInstance.off('message_recieved');
         };
     }, [params.roomID]);
 
-    const sendMsg = (): void => {
+    // recieve messages logic
+    useEffect(() => {
+        socket?.on('message_recieved', (newMessage: string) => {
+            const copyMessages = [...messages, newMessage];
+            setMessages(copyMessages);
+        });
+
+        return () => {
+            socket?.off('message_recieved');
+        };
+    }, [socket, messages]);
+
+    const sendMessage = (message: string): void => {
         socket?.emit('message', message);
-        setMessage('');
     };
 
     return (
@@ -40,10 +58,13 @@ function ChatRoom(): JSX.Element {
 
             {joinStatus.joined ? (
                 <>
-                    <input value={message} onChange={(e) => setMessage(e.target.value)} />
-                    <button type="button" onClick={sendMsg}>
-                        send
-                    </button>
+                    <div>
+                        {messages.length > 0 &&
+                            messages.map((userMessage) => {
+                                return <p>{userMessage}</p>;
+                            })}
+                    </div>
+                    <MessageInput sendMessage={sendMessage} />
                 </>
             ) : (
                 <p>{joinStatus.message}</p>
---
+++ src/components/ChatRoom/MessageInput/MessageInput.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Input } from '../../../utilComponents';
+
+interface IMessageInputProps {
+    sendMessage: (message: string) => void;
+}
+
+function MessageInput(props: IMessageInputProps): JSX.Element {
+    const { sendMessage } = props;
+    const [message, setMessage] = useState('');
+
+    const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setMessage(e.target.value);
+    };
+
+    const handleSendMessage = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        sendMessage(message);
+        setMessage('');
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSendMessage}>
+                <Input required value={message} onChange={handleMessageChange} />
+                <button type="submit">send</button>
+            </form>
+        </div>
+    );
+}
+
+export default MessageInput;
